750c7417c9efeb93a99831809aa845c7e0970483
YYC <455369054@qq.com>
2018-01-15 13:49:12 +0800

    update

--- doc2docx.py
+++ doc2docx.py
@@ -4,13 +4,20 @@
 from win32com import client as wc
 import os
 import shutil
+import datetime
 
 word = wc.Dispatch('Word.Application')
 
-src_path = r"F:\pythoncode\score\src_doc"
-dst_path = r'F:\pythoncode\score\dst_docx'
+src_path_56 = r"F:\pythoncode\score\src_doc_56"
+dst_path_56 = r'F:\pythoncode\score\dst_docx_56'
+src_path_78 = r"F:\pythoncode\score\src_doc_78"
+dst_path_78 = r'F:\pythoncode\score\dst_docx_78'
 
 def doc2docx(src_dir, dst_dir):
+    if(os.path.exists(dst_dir)):
+        shutil.rmtree(dst_dir)
+    os.mkdir(dst_dir)
+
     i = 0
     j = 0
     for path, subdirs, files in os.walk(src_dir):
@@ -38,7 +45,6 @@ def doc2docx(src_dir, dst_dir):
                     j += 1
                     print(wordFullName + ':该文件保存失败****************************************')
 
-    word.Quit()
     print('尝试转换' + str(i) + '个docx')
     print('其中成功的有：' + str(i - j) + '个')
     print('失败的共有:' + str(j) + '个')
@@ -48,6 +54,11 @@ def doc2docx(src_dir, dst_dir):
 # word.Visible = True #是否可见
 # word.DisplayAlerts = 0
 def docx2doc(src_dir, dst_dir):
+
+    if(os.path.exists(dst_dir)):
+        shutil.rmtree(dst_dir)
+    os.mkdir(dst_dir)
+
     i = 0
     j = 0
     for path, subdirs, files in os.walk(src_dir):
@@ -76,12 +87,24 @@ def docx2doc(src_dir, dst_dir):
                     print
                     wordFullName + ':该文件保存失败****************************************'
 
-    word.Quit()
     print('尝试转换' + str(i) + '个docx')
     print('其中成功的有：' + str(i - j) + '个')
     print('失败的共有:' + str(j) + '个')
 
 
 if __name__ == '__main__':
+    starttime = datetime.datetime.now()
+    print('Start time is %s.' % (str(datetime.datetime.now())))
+
     #docx2doc(r"H:\My\stock\doc")
-    doc2docx(src_path, dst_path)
+
+    doc2docx(src_path_56, dst_path_56)
+
+    doc2docx(src_path_78, dst_path_78)
+
+    word.Quit()
+
+    # 程序结束时间 及 耗时
+    timedelta = datetime.datetime.now() - starttime
+    print('End time is %s.' % (str(datetime.datetime.now())))
+    print('Total test execution duration is %s.' % (timedelta.__str__()))
--- homework.py
+++ homework.py
@@ -14,10 +14,13 @@ import xlrd
 import xlwt
 from xlutils.copy import copy
 
-docx_path = 'F:\\pythoncode\\score\\dst_docx\\' #要分析的文件目录
-student_idname = 'F:\\pythoncode\\score\\jk152078.xls'
-score_fiel = 'F:\\pythoncode\\score\\jk152078score.xls'
+docx_path_56 = 'F:\\pythoncode\\score\\dst_docx_56\\' #要分析的文件目录
+student_idname_56 = 'F:\\pythoncode\\score\\jk152056.xls'
+score_fiel_56 = 'F:\\pythoncode\\score\\jk152056score.xls'
 
+docx_path_78 = 'F:\\pythoncode\\score\\dst_docx_78\\' #要分析的文件目录
+student_idname_78 = 'F:\\pythoncode\\score\\jk152078.xls'
+score_fiel_78 = 'F:\\pythoncode\\score\\jk152078score.xls'
 
 def get_max_min_size():
     min = 0
@@ -65,7 +68,7 @@ def get_score_from_file(file_name, max, min):
 
 
 
-def parase_all_and_record_score():
+def parase_all_and_record_score(docx_path,student_idname,score_fiel):
     #打开学生名单excel，copy一份另存并写入成绩
     source_excel_file = xlrd.open_workbook(student_idname)
     dest_excel_file = copy(source_excel_file)
@@ -94,7 +97,7 @@ def parase_all_and_record_score():
 
     dest_excel_file.save(score_fiel)
 
-def parase_all_and_insert_db():
+def parase_all_and_insert_db(docx_path):
     clear_table('words_temp')  # 清除数据
     operatMySQl = OperateMySQL()
 
@@ -102,6 +105,7 @@ def parase_all_and_insert_db():
 
     # 获取文本内容
     contents_list = []
+    words_list = []
     file_list = os.listdir(docx_path)
     for filename in file_list:
         filepathname= docx_path + filename
@@ -112,11 +116,10 @@ def parase_all_and_insert_db():
         for paragraph in document.paragraphs:
             contents_list.append(paragraph.text.encode('utf-8'))
         # 分词
-        words_list = []
         for text in contents_list:
             splits = jieba.cut(text)
             for word in splits:
-                words_list.append(word.lower())
+                words_list.append(word.strip().lower())
 
     # 统计分词频率，计入数据库
     counter = collections.Counter(words_list)
@@ -130,9 +133,13 @@ def parase_all_and_insert_db():
 
 
     # 清洗无用数据
-    clean_date_table = ['的', ' ', '\t', '\n', '、', '.', '，', '/', '；', '（', '）']
+    clean_date_table = [ ' ', '\t', '\n', '、', '.', '，', '/', '；', '（', '）','。','：',',','-',';',')',':',
+                        '的', '和','熟悉','开发','等','有','相关','及','能力','优先','工作','良好','与','对','年','常用',
+                        '能够','了解','者','专业','熟练','能','计算机','专业','电子','进行',
+                        '以上','具有','使用','精通','或','嵌入式','工程师',
+                        ' ','1','2','3','4','5','6','7','8','9','0']
+    sqli = "delete from words_temp  where words_content = '{0}';"
     for clean_item in clean_date_table:
-        sqli = "delete from words_temp  where words_content = '{0}';"
         sqlm = sqli.format(clean_item)
         operatMySQl.execute(sqlm)
     operatMySQl.commit()
@@ -142,9 +149,13 @@ if __name__ == '__main__':
     starttime = datetime.datetime.now()
     print('Start time is %s.' % (str(datetime.datetime.now())))
 
-    parase_all_and_insert_db()
+    parase_all_and_insert_db(docx_path_56)
 
-    #parase_all_and_record_score()
+    parase_all_and_record_score(docx_path_56,student_idname_56,score_fiel_56)
+
+    #parase_all_and_insert_db(docx_path_78)
+
+    #parase_all_and_record_score(docx_path_78,student_idname_78,score_fiel_78)
 
     # 程序结束时间 及 耗时
     timedelta = datetime.datetime.now() - starttime
